851f8b94d459d06714d17fe0f7edf56fd323ebbe
Texghn Gordon <texghng@outlook.com>
2021-11-25 15:26:24 -0700

    RecetaPlus
    A different look

---
+++ RecetaPlus/MainForm.Designer.cs
@@ -0,0 +1,255 @@
+﻿
+namespace RecetaPlus
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.pnlMenu = new System.Windows.Forms.Panel();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.btnJuice = new System.Windows.Forms.Button();
+            this.btnDesert = new System.Windows.Forms.Button();
+            this.btnDinner = new System.Windows.Forms.Button();
+            this.btnLunch = new System.Windows.Forms.Button();
+            this.btnBreakFast = new System.Windows.Forms.Button();
+            this.pnlLogo = new System.Windows.Forms.Panel();
+            this.pnlTitleBar = new System.Windows.Forms.Panel();
+            this.lblHome = new System.Windows.Forms.Label();
+            this.txtBoxSearch = new System.Windows.Forms.TextBox();
+            this.pnlMenu.SuspendLayout();
+            this.pnlTitleBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlMenu
+            // 
+            this.pnlMenu.BackColor = System.Drawing.Color.White;
+            this.pnlMenu.Controls.Add(this.btnHelp);
+            this.pnlMenu.Controls.Add(this.btnJuice);
+            this.pnlMenu.Controls.Add(this.btnDesert);
+            this.pnlMenu.Controls.Add(this.btnDinner);
+            this.pnlMenu.Controls.Add(this.btnLunch);
+            this.pnlMenu.Controls.Add(this.btnBreakFast);
+            this.pnlMenu.Controls.Add(this.pnlLogo);
+            this.pnlMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlMenu.Location = new System.Drawing.Point(0, 0);
+            this.pnlMenu.Name = "pnlMenu";
+            this.pnlMenu.Size = new System.Drawing.Size(220, 595);
+            this.pnlMenu.TabIndex = 0;
+            // 
+            // btnHelp
+            // 
+            this.btnHelp.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnHelp.FlatAppearance.BorderSize = 0;
+            this.btnHelp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHelp.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHelp.ForeColor = System.Drawing.Color.DarkGreen;
+            this.btnHelp.Image = ((System.Drawing.Image)(resources.GetObject("btnHelp.Image")));
+            this.btnHelp.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnHelp.Location = new System.Drawing.Point(0, 403);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btnHelp.Size = new System.Drawing.Size(220, 60);
+            this.btnHelp.TabIndex = 6;
+            this.btnHelp.Text = "  Help";
+            this.btnHelp.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnHelp.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnHelp.UseVisualStyleBackColor = true;
+            // 
+            // btnJuice
+            // 
+            this.btnJuice.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnJuice.FlatAppearance.BorderSize = 0;
+            this.btnJuice.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnJuice.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnJuice.ForeColor = System.Drawing.Color.DarkGreen;
+            this.btnJuice.Image = ((System.Drawing.Image)(resources.GetObject("btnJuice.Image")));
+            this.btnJuice.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnJuice.Location = new System.Drawing.Point(0, 343);
+            this.btnJuice.Name = "btnJuice";
+            this.btnJuice.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btnJuice.Size = new System.Drawing.Size(220, 60);
+            this.btnJuice.TabIndex = 5;
+            this.btnJuice.Text = "  Juice";
+            this.btnJuice.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnJuice.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnJuice.UseVisualStyleBackColor = true;
+            this.btnJuice.Click += new System.EventHandler(this.btnJuice_Click);
+            // 
+            // btnDesert
+            // 
+            this.btnDesert.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnDesert.FlatAppearance.BorderSize = 0;
+            this.btnDesert.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDesert.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDesert.ForeColor = System.Drawing.Color.DarkGreen;
+            this.btnDesert.Image = ((System.Drawing.Image)(resources.GetObject("btnDesert.Image")));
+            this.btnDesert.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDesert.Location = new System.Drawing.Point(0, 283);
+            this.btnDesert.Name = "btnDesert";
+            this.btnDesert.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btnDesert.Size = new System.Drawing.Size(220, 60);
+            this.btnDesert.TabIndex = 4;
+            this.btnDesert.Text = "  Desert";
+            this.btnDesert.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDesert.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnDesert.UseVisualStyleBackColor = true;
+            // 
+            // btnDinner
+            // 
+            this.btnDinner.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnDinner.FlatAppearance.BorderSize = 0;
+            this.btnDinner.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDinner.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDinner.ForeColor = System.Drawing.Color.DarkGreen;
+            this.btnDinner.Image = ((System.Drawing.Image)(resources.GetObject("btnDinner.Image")));
+            this.btnDinner.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDinner.Location = new System.Drawing.Point(0, 223);
+            this.btnDinner.Name = "btnDinner";
+            this.btnDinner.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btnDinner.Size = new System.Drawing.Size(220, 60);
+            this.btnDinner.TabIndex = 3;
+            this.btnDinner.Text = "  Dinner";
+            this.btnDinner.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnDinner.UseVisualStyleBackColor = true;
+            // 
+            // btnLunch
+            // 
+            this.btnLunch.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnLunch.FlatAppearance.BorderSize = 0;
+            this.btnLunch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLunch.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLunch.ForeColor = System.Drawing.Color.DarkGreen;
+            this.btnLunch.Image = ((System.Drawing.Image)(resources.GetObject("btnLunch.Image")));
+            this.btnLunch.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLunch.Location = new System.Drawing.Point(0, 163);
+            this.btnLunch.Name = "btnLunch";
+            this.btnLunch.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btnLunch.Size = new System.Drawing.Size(220, 60);
+            this.btnLunch.TabIndex = 2;
+            this.btnLunch.Text = "  Lunch";
+            this.btnLunch.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLunch.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnLunch.UseVisualStyleBackColor = true;
+            // 
+            // btnBreakFast
+            // 
+            this.btnBreakFast.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnBreakFast.FlatAppearance.BorderSize = 0;
+            this.btnBreakFast.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBreakFast.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBreakFast.ForeColor = System.Drawing.Color.DarkGreen;
+            this.btnBreakFast.Image = ((System.Drawing.Image)(resources.GetObject("btnBreakFast.Image")));
+            this.btnBreakFast.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnBreakFast.Location = new System.Drawing.Point(0, 103);
+            this.btnBreakFast.Name = "btnBreakFast";
+            this.btnBreakFast.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btnBreakFast.Size = new System.Drawing.Size(220, 60);
+            this.btnBreakFast.TabIndex = 1;
+            this.btnBreakFast.Text = "  Break Fast";
+            this.btnBreakFast.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnBreakFast.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnBreakFast.UseVisualStyleBackColor = true;
+            // 
+            // pnlLogo
+            // 
+            this.pnlLogo.BackColor = System.Drawing.Color.Bisque;
+            this.pnlLogo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pnlLogo.BackgroundImage")));
+            this.pnlLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pnlLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlLogo.Location = new System.Drawing.Point(0, 0);
+            this.pnlLogo.Name = "pnlLogo";
+            this.pnlLogo.Size = new System.Drawing.Size(220, 103);
+            this.pnlLogo.TabIndex = 0;
+            // 
+            // pnlTitleBar
+            // 
+            this.pnlTitleBar.BackColor = System.Drawing.Color.White;
+            this.pnlTitleBar.Controls.Add(this.txtBoxSearch);
+            this.pnlTitleBar.Controls.Add(this.lblHome);
+            this.pnlTitleBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTitleBar.Location = new System.Drawing.Point(220, 0);
+            this.pnlTitleBar.Name = "pnlTitleBar";
+            this.pnlTitleBar.Size = new System.Drawing.Size(882, 103);
+            this.pnlTitleBar.TabIndex = 1;
+            // 
+            // lblHome
+            // 
+            this.lblHome.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblHome.AutoSize = true;
+            this.lblHome.Font = new System.Drawing.Font("Arial Rounded MT Bold", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHome.ForeColor = System.Drawing.Color.DarkGreen;
+            this.lblHome.Location = new System.Drawing.Point(362, 22);
+            this.lblHome.Name = "lblHome";
+            this.lblHome.Size = new System.Drawing.Size(100, 32);
+            this.lblHome.TabIndex = 0;
+            this.lblHome.Text = "HOME";
+            // 
+            // txtBoxSearch
+            // 
+            this.txtBoxSearch.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBoxSearch.Location = new System.Drawing.Point(230, 67);
+            this.txtBoxSearch.Name = "txtBoxSearch";
+            this.txtBoxSearch.Size = new System.Drawing.Size(380, 22);
+            this.txtBoxSearch.TabIndex = 1;
+            this.txtBoxSearch.Text = "Search";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1102, 595);
+            this.Controls.Add(this.pnlTitleBar);
+            this.Controls.Add(this.pnlMenu);
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.pnlMenu.ResumeLayout(false);
+            this.pnlTitleBar.ResumeLayout(false);
+            this.pnlTitleBar.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlMenu;
+        private System.Windows.Forms.Button btnDesert;
+        private System.Windows.Forms.Button btnDinner;
+        private System.Windows.Forms.Button btnLunch;
+        private System.Windows.Forms.Button btnBreakFast;
+        private System.Windows.Forms.Panel pnlLogo;
+        private System.Windows.Forms.Button btnHelp;
+        private System.Windows.Forms.Button btnJuice;
+        private System.Windows.Forms.Panel pnlTitleBar;
+        private System.Windows.Forms.Label lblHome;
+        private System.Windows.Forms.TextBox txtBoxSearch;
+    }
+}
+
